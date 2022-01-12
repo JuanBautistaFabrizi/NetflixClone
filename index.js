5b391b8ca1fb8c32805ee2a470ba6117183994f4
juan bautista fabrizi <juanbautistafabrizi@gmail.com>
2022-01-12 20:04:53 -0300

    body-parser added

--- index.js
+++ index.js
@@ -3,15 +3,20 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
+const bodyParser= require('body-parser');
 
 dotenv.config();
 
-//mongoose.connect(process.env.MONGO_URL,{useNewUrlParser: true, useUnifiedTopology: true});
-main().catch(err => console.log(err));
+//body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect(process.env.MONGO_URL,{useNewUrlParser: true, useUnifiedTopology: true});
+/* main().catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect(process.env.MONGO_URL);
-}
+} */
 
 app.use(express.json());
 
--- routes/auth.js
+++ routes/auth.js
@@ -2,14 +2,15 @@ const router = require("express").Router();
 const { json } = require("express/lib/response");
 const User = require("../models/User");
 
+
+
 //Register
 router.post("/register",async (req,res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
         password: req.body.password
-
-    })
+    });
     try {
         const user = await newUser.save()
         res.status(201).json(user);
