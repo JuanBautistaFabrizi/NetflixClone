67575f91f0733694441364b12d47978d5222282b
juan bautista fabrizi <juanbautistafabrizi@gmail.com>
2022-01-13 16:51:08 -0300

    CRUD done

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const authRoute = require("./routes/auth");
 const bodyParser= require('body-parser');
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 dotenv.config();
 
@@ -21,6 +22,7 @@ async function main() {
 app.use(express.json());
 
 app.use("/api/auth",authRoute);
+app.use("/api/users",userRoute);
 
 app.listen(3000,() => {
     console.log("backend server is running!");
