9658df61404c6d1e4055dbdad938986e88023501
juan bautista fabrizi <juanbautistafabrizi@gmail.com>
2022-01-17 11:44:37 -0300

    movie route added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const bodyParser= require('body-parser');
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
 
 dotenv.config();
 
@@ -23,6 +24,7 @@ app.use(express.json());
 
 app.use("/api/auth",authRoute);
 app.use("/api/users",userRoute);
+app.use("/api/movies",movieRoute);
 
 app.listen(3000,() => {
     console.log("backend server is running!");
